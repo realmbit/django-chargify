67adc333583fd2cde5db1649468f7aacacf1b2b4
Lie Ryan <lie.1296@gmail.com>
2011-09-12 16:24:45 +1000

    add feature to allow creating one-time charge through Subscription

--- chargify/pychargify/api.py
+++ chargify/pychargify/api.py
@@ -600,6 +600,13 @@ class ChargifySubscription(ChargifyBase):
 
         self._delete("/subscriptions/" + self.id + ".xml", xml)
 
+    def charge(self, amount, memo):
+        xml = """<?xml version="1.0" encoding="UTF-8"?>
+<charge>
+  <amount>%s</amount>
+  <memo>%s</memo>
+</charge>""" % (amount, memo)
+        self._post('/subscriptions/' + self.id + '/charges.xml', xml)
 
 class ChargifyCreditCard(ChargifyBase):
     """
